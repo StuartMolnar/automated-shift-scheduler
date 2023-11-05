deedff0abdcb072ea30670db6308200641dcfea2
StuartMolnar <stuartmolnar@hotmail.com>
2023-11-04 22:34:44 -0700

    Update database schema

--- database/database.py
+++ database/database.py
@@ -3,13 +3,12 @@ import sqlite3
 conn = sqlite3.connect('scheduling.db')
 cursor = conn.cursor()
 
-# Create the Employees Table
+# Create the Roles Table
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Employees (
-    EmployeeID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Name TEXT NOT NULL,
-    Email TEXT UNIQUE NOT NULL,
-    Role TEXT NOT NULL,
+CREATE TABLE IF NOT EXISTS Roles (
+    RoleID INTEGER PRIMARY KEY AUTOINCREMENT,
+    RoleName TEXT UNIQUE NOT NULL,
+    Description TEXT,
     CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
     UpdatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
 );
@@ -19,37 +18,53 @@ CREATE TABLE IF NOT EXISTS Employees (
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Shifts (
     ShiftID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Role TEXT NOT NULL,
+    RoleID INTEGER NOT NULL,
     Description TEXT,
-    StartTime TEXT NOT NULL,
-    EndTime TEXT NOT NULL,
-    CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-    UpdatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-''')
-
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS EmployeeShifts (
-    EmployeeShiftID INTEGER PRIMARY KEY AUTOINCREMENT,
-    EmployeeID INTEGER NOT NULL,
-    ShiftID INTEGER NOT NULL,
-    PreferenceLevel INTEGER NOT NULL,
+    StartTime DATETIME NOT NULL,
+    EndTime DATETIME NOT NULL,
+    EmployeeID INTEGER,
     CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
     UpdatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (EmployeeID) REFERENCES Employees (EmployeeID),
-    FOREIGN KEY (ShiftID) REFERENCES Shifts (ShiftID)
+    FOREIGN KEY (RoleID) REFERENCES Roles (RoleID),
+    FOREIGN KEY (EmployeeID) REFERENCES Employees (EmployeeID)
 );
 ''')
 
-# Create the IdealSchedules Table
+# Create the Employees Table
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS IdealSchedules (
-    IdealScheduleID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Role TEXT NOT NULL,
-    ScheduleDetails TEXT NOT NULL, -- Consider JSON or another structured format
-    CreatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    IsValid BOOLEAN NOT NULL DEFAULT TRUE
-)
+CREATE TABLE IF NOT EXISTS Employees (
+    EmployeeID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL,
+    Email TEXT UNIQUE NOT NULL,
+    RoleID INTEGER,
+    CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    UpdatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (RoleID) REFERENCES Roles (RoleID)
+);
+''')
+
+# Create the Availability Table
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS EmployeeAvailability (
+    AvailabilityID INTEGER PRIMARY KEY AUTOINCREMENT,
+    EmployeeID INTEGER NOT NULL,
+    DayOfWeek INTEGER NOT NULL,  -- 0 = Sunday, 1 = Monday, etc.
+    StartTime TIME NOT NULL,
+    EndTime TIME NOT NULL,
+    FOREIGN KEY (EmployeeID) REFERENCES Employees (EmployeeID)
+);
+''')
+
+# Create the Preference Table
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS EmployeePreferences (
+    PreferenceID INTEGER PRIMARY KEY AUTOINCREMENT,
+    EmployeeID INTEGER NOT NULL,
+    ShiftPatternID INTEGER,
+    PreferenceLevel INTEGER NOT NULL,  -- Example: 1 = High, 2 = Medium, 3 = Low
+    FOREIGN KEY (EmployeeID) REFERENCES Employees (EmployeeID),
+    FOREIGN KEY (ShiftPatternID) REFERENCES ShiftPatterns (PatternID)
+);
 ''')
 
 # Commit the changes
